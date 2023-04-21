a75b4d928153f3b15e61a1d8dac312f37d65a806
WoodHolz <1535980162@qq.com>
2023-04-21 16:44:45 +0800

    final ch3

--- os/syscall.c
+++ os/syscall.c
@@ -45,8 +45,12 @@ uint64 sys_task_info(TaskInfo *ti)
 	uint64 cycle = get_cycle();
 	printf("\n**********\n%d\n**********\n", cycle);
 	ti->status = Running; // Emmm 
+	/* 可能精度会受到影响
 	ti->time = ((cycle % CPU_FREQ) * 1000 / CPU_FREQ) - \
 		((curr_proc()->str_time % CPU_FREQ) * 1000 / CPU_FREQ);
+	*/
+	ti->time = cycle * 1000 / CPU_FREQ - \
+		curr_proc()->str_time *1000 / CPU_FREQ;
 	int i = 0;
 	while (i < MAX_SYSCALL_NUM)
 	{
